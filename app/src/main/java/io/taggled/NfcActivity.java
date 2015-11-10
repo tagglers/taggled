a839ec189e9bd46da0b9d31a366bc052a56f3c17
Gordon Yoon <gordonyoon@gmail.com>
2015-11-09 22:16:40 -0800

    Implement Butterknife into NfcActivity

--- app/src/main/java/io/taggled/NfcActivity.java
+++ app/src/main/java/io/taggled/NfcActivity.java
@@ -1,28 +1,33 @@
 package io.taggled;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.OnClick;
+
+import static butterknife.ButterKnife.bind;
+import static butterknife.ButterKnife.findById;
+
 public class NfcActivity extends AppCompatActivity {
+
+    private static final String TAG = "NfcActivity";
+
+    @OnClick(R.id.fab)
+    void onClickFab(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Toolbar toolbar = findById(this, R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
