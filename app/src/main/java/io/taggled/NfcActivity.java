4428c18b5f4ba107dbf38ef32111c74bf7e5ef4f
Gordon Yoon <gordonyoon@gmail.com>
2015-11-09 22:48:24 -0800

    Implement Android Beam send & receive

--- app/src/main/java/io/taggled/NfcActivity.java
+++ app/src/main/java/io/taggled/NfcActivity.java
@@ -1,9 +1,16 @@
 package io.taggled;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import butterknife.OnClick;
@@ -11,9 +18,11 @@ import butterknife.OnClick;
 import static butterknife.ButterKnife.bind;
 import static butterknife.ButterKnife.findById;
 
-public class NfcActivity extends AppCompatActivity {
+public class NfcActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
 
     private static final String TAG = "NfcActivity";
+
+    NfcAdapter mNfcAdapter;
 
     @OnClick(R.id.fab)
     void onClickFab(View view) {
@@ -29,5 +38,58 @@ public class NfcActivity extends AppCompatActivity {
 
         Toolbar toolbar = findById(this, R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setupNfcAdapter();
+    }
+
+    /**
+     * Register to send messages via Android Beam
+     *
+     * @return registered successfully
+     */
+    private boolean setupNfcAdapter() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter != null) {
+            mNfcAdapter.setNdefPushMessageCallback(this, this);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // indicates that the activity was started by an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    private void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage msg = (NdefMessage) rawMsgs[0];  // get the mime type
+        printNdef(msg.getRecords());
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        return new NdefMessage(new NdefRecord[]{
+                NdefRecord.createMime(getString(R.string.mime_type), getUserId().getBytes()),
+                NdefRecord.createApplicationRecord("io.taggled")});
+    }
+
+    private String getUserId() {
+        // TODO: get unique user id from the backend
+        return "12345";
+    }
+
+    private void printNdef(NdefRecord[] records) {
+        if (records == null) return;
+
+        for (int i = 0; i < records.length; i++) {
+            Log.d(TAG, "RECORD " + i + " PAYLOAD: " + new String(records[i].getPayload()));
+            Log.d(TAG, "RECORD " + i + " MIME TYPE: " + records[i].toMimeType());
+        }
     }
 }
