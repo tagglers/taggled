34363b7ac49526626103dced6b570d539d01b1f9
Gordon Yoon <gordonyoon@gmail.com>
2015-11-10 12:47:50 -0800

    Implement initial client side gcm

---
+++ app/src/main/java/io/taggled/gcm/MyGcmListenerService.java
@@ -0,0 +1,25 @@
+package io.taggled.gcm;
+
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class MyGcmListenerService extends GcmListenerService {
+
+    private static final String TAG = "MyGcmListenerService";
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Message: " + message);
+
+        if (from.startsWith("/topics/")) {
+            // message received from some topic.
+        } else {
+            // normal downstream message.
+        }
+    }
+}
