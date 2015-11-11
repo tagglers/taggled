f27009349104801721ffab7e51ace232b2a27c35
sanyukta <sanyukta22@gmail.com>
2015-11-10 22:00:43 -0800

    Update san

--- app/src/main/java/io/taggled/CampaignDetailActivity.java
+++ app/src/main/java/io/taggled/CampaignDetailActivity.java
@@ -1,20 +1,41 @@
 package io.taggled;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CampaignDetailActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign_detail);
+
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager)findViewById(R.id.container_pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
 
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,7 +45,69 @@ public class CampaignDetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test_swipe, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position)
+            {
+                case 0: return CampaignDetailActivityFragment.newInstance("test");
+                case 1: return CampaignDetailUpdateActivityFragment.newInstance("test2");
+                default:return CampaignDetailActivityFragment.newInstance("test");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "CAMPAIGN";
+                case 1:
+                    return "CAMPAIGN UPDATES";
+            }
+            return null;
+        }
     }
 
 }
--- app/src/main/java/io/taggled/CampaignDetailActivityFragment.java
+++ app/src/main/java/io/taggled/CampaignDetailActivityFragment.java
@@ -1,18 +1,10 @@
 package io.taggled;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import io.taggled.models.Campaign;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,46 +15,27 @@ public class CampaignDetailActivityFragment extends Fragment {
     }
 
 
-    private List<Campaign> campaigns;
-    private RecyclerView rv_campaigns_updates;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v =inflater.inflate(R.layout.fragment_campaign_detail, container, false);
-        rv_campaigns_updates=(RecyclerView)v.findViewById(R.id.rv_campaigns_updates);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        rv_campaigns_updates.setLayoutManager(linearLayoutManager);
-        rv_campaigns_updates.setHasFixedSize(true);
-
-        initializeData();
-        initializeAdapter();
 
         return v;
     }
+    public static CampaignDetailActivityFragment newInstance(String text) {
 
+        CampaignDetailActivityFragment f = new CampaignDetailActivityFragment();
+//        Bundle b = new Bundle();
+//        b.putString("msg", text);
+//
+//        f.setArguments(b);
 
-
-
-    private void initializeData(){
-        campaigns = new ArrayList<Campaign>();
-
-        Campaign campaign = new Campaign();
-
-        campaign.setEndDate(new Date());
-        campaign.setName("Drinking Water for Africa");
-        campaign.setTargetAmount(40000);
-        campaigns.add(campaign);
-        campaigns.add(campaign);
-        campaigns.add(campaign);
-        campaigns.add(campaign);
+        return f;
     }
 
-    private void initializeAdapter(){
-        RV_Campaign_Updates_Adapter adapter = new RV_Campaign_Updates_Adapter(campaigns);
-        rv_campaigns_updates.setAdapter(adapter);
-    }
+
+
 }
 
